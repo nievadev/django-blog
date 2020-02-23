ac060ae18c793e29a0198915848fea668ddc0013
Martín Nieva <martinjafactor@gmail.com>
2020-02-23 03:30:01 -0300

    Added CreateView, UpdateView and DetailView

--- users/signals.py
+++ users/signals.py
@@ -12,9 +12,9 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        print("Created!")
+        print(f'Created profile for {instance.username}!')
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-    print("Saved!")
+    print(f'Saved {instance.username}\'s profile!')
