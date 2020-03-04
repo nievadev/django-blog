b9202d10f667b550f5c33f156dfbd27a9fbe5a9b
Martín Nieva <martinjafactor@gmail.com>
2020-03-04 02:39:02 -0300

    removed env from project

--- django_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/martin/programming/django/django_project/django_env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
